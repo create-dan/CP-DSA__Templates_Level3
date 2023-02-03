06058121b261d4d88db083eb00e75c6f84587803
DNYANESHWAR SHINDE <85820206+create-dan@users.noreply.github.com>
2023-02-03 15:23:35 +0530

    Create Maximum Subarray Kadane.cpp

---
+++ Striver's Sheet/Maximum Subarray Kadane.cpp
@@ -0,0 +1,47 @@
+/*
+    Time Complexity: O(N^3)
+    Space Complexity: O(1)
+
+    where N is the length of the array.
+*/
+
+#include <algorithm>
+
+long long maxSubarraySum(int arr[], int n)
+{
+    long long maxSum = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i; j<n; j++)
+        {
+            long long curSum = 0;
+            
+            for(int k=i; k<=j; k++)
+            {
+                curSum += arr[k];
+            }
+
+            maxSum = max(maxSum, curSum);
+        }
+    }
+
+    return maxSum;
+}
+
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr=nums[0],ans=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+           curr = max(nums[i],curr+nums[i]);
+            ans = max(ans,curr);
+        }
+        
+        return ans;
+    }
+};
+
